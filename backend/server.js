9041e89daa40c7b892637de60a4b22860d10b3a7
arif-07-09 <157058358+arif-07-09@users.noreply.github.com>
2025-07-03 18:35:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,10 @@ const notificationRoutes = require('./routes/notifications');
 
 const app = express();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // ✅ serve images
-// ✅ Middleware
-app.use(cors());
+// ✅ Middlewareapp.use(cors({
+  origin: "https://devconnect-frontend-ekwg.vercel.app", // allow your frontend
+  credentials: true // only if using cookies or sessions
+}));
 app.use(express.json());           // Parse JSON requests
 app.use(express.urlencoded({ extended: true })); // Parse form data
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
