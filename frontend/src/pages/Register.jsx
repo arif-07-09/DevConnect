58a7bd992ae4568346fd815a08f72eda645072c3
arif-07-09 <157058358+arif-07-09@users.noreply.github.com>
2025-07-03 21:44:54 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -9,7 +9,7 @@ const Register = () => {
   const [message, setMessage] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
-  const API = "https://devconnect-2-l0gv.onrender.com"; // ⬅️ update this
+  const API = process.env.REACT_APP_API_BASE; // ⬅️ update this
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
