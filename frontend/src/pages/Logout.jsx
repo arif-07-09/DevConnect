036edb4711c9e6d14135fe3b226d8d82106b7160
arif-07-09 <157058358+arif-07-09@users.noreply.github.com>
2025-07-04 19:26:48 +0530

    Update Logout.jsx

--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -11,7 +11,20 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
       <Link className="navbar-brand" to="/">DevConnect</Link>
-      <div className="collapse navbar-collapse">
+
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
             <Link className="nav-link" to="/profile">Profile</Link>
